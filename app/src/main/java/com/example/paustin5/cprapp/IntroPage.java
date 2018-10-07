e4d7d2b7297520fd1d90da5848374016b1c9f24f
jairorubio <jairorubio@tuta.io>
2018-10-06 20:26:36 -0400

    intro page added

---
+++ app/src/main/java/com/example/paustin5/cprapp/IntroPage.java
@@ -0,0 +1,13 @@
+package com.example.paustin5.cprapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class IntroPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intropage);
+    }
+}
