69dc3d71bdfd03d6d3dcc92920354197bf2f68ef
bryce-koester <bryce@thekoesters.org>
2018-10-07 09:45:41 -0400

    fixed main error

--- app/src/main/java/com/example/paustin5/cprapp/MainActivity.java
+++ app/src/main/java/com/example/paustin5/cprapp/MainActivity.java
@@ -4,22 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.util.Timer;
-import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
-    Timer timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, BeforeCPR1.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 5000);
+
     }
 }
